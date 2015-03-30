c2aaa41bfba99e9579b1150771675b0fa0ca7c9b
Matthew Astley <mca@sanger.ac.uk>
2015-03-30 11:35:50 +0100

    release under AGPLv3
    following local employer (CGP) convention
    
    This is compatible with Plack, which offers itself under
    
    | Terms of the Perl programming language system itself
    | a) the GNU General Public License as published by the Free
    |    Software Foundation; either version 1, or (at your option) any
    |    later version, or
    | b) the "Artistic License"
    
    , via GPLv3 at least

--- lib/Plack/MiddlewareX/PreZipped.pm
+++ lib/Plack/MiddlewareX/PreZipped.pm
@@ -46,6 +46,28 @@ JBrowse.
 
 There is no test suite.
 
+=head1 LICENCE
+
+ This file is part of plack-middlewares-jbrowse which extends Plack to
+ support the needs of JBrowse.
+
+ Copyright (c) 2015 Genome Research Ltd.
+
+ Author: Matthew Astley <mca@sanger.ac.uk>
+
+ This program is free software: you can redistribute it and/or modify it
+ under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or (at your
+ option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 =cut
 
 
